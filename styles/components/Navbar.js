b1a6e257f94491ad2e8cf1992b380d221d39711a
ridhoarazzak <54082062+ridhoarazzak@users.noreply.github.com>
2025-06-28 00:55:20 +0700

    Create Navbar.js

---
+++ styles/components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-[#6F4E37] text-white p-4 flex justify-between items-center shadow-md">
+      <Link href="/" className="text-xl font-bold">☕ Motel & Tourism</Link>
+      <div className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/hotels">Hotel</Link>
+        <Link href="/tourism">Wisata</Link>
+        <Link href="/galeri">Galeri</Link>
+        <Link href="/contact">Kontak</Link>
+      </div>
+    </nav>
+  );
+}
